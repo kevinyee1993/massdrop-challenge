93beb8e728f93790d61d50d5593f9b95b1015c61
Kevin Yee <kevinyee1993@gmail.com>
2018-06-05 21:03:52 -0700

    Set up get and post with body parser

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+// tells body parser to extract data from form element and adds them
+// to the body property of the request object
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function() {
   console.log("listening to port 3000");
+});
+
+
+// can use this to get the url they send in
+app.get('/', function(req, res) {
+  // res.send('This is a get request');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/todo', function(req, res) {
+  console.log(req.body);
 });
